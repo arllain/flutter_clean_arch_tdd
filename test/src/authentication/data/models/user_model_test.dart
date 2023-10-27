87c72f1bde63c1c2b7d31d87a4df809de534261a
Arllain <arllain@gmail.com>
2023-10-27 18:52:06 -0300

    [Data Layer] run test toJson, fromMap, toMap

--- test/src/authentication/data/models/user_model_test.dart
+++ test/src/authentication/data/models/user_model_test.dart
@@ -22,8 +22,45 @@ void main() {
     test('should return a [UserModel] with the right data', () {
       //Act
       final result = UserModel.fromMap(tMap);
-      expect(result, equals(tModel));
+
       //Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('fromJson', () {
+    test('should return a [UserModel] with the right data', () {
+      //Act
+      final result = UserModel.fromJson(tJson);
+
+      //Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('toMap', () {
+    test('should return a [Map] with the right data', () {
+      //Act
+      final result = tModel.toMap();
+
+      //Assert
+      expect(result, equals(tMap));
+    });
+  });
+
+  group('toJson', () {
+    test('should return a [JSON] with the right data', () {
+      //Act
+      final result = tModel.toJson();
+      final tJson = jsonEncode({
+        "id": "1",
+        "createdAt": "_empty.createdAt",
+        "name": "_empty.name",
+        "avatar": "_empty.avatar"
+      });
+
+      //Assert
+      expect(result, equals(tJson));
     });
   });
 }
